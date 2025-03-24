14ebd0a62fa69825f92cc5d96588c303abb561b6
MSVT <ahmedmsvt@gmail.com>
2025-03-24 02:33:55 +0200

    Update ddos.py

--- ddos.py
+++ ddos.py
@@ -1,14 +1,1 @@
-from flask import Flask
-import time
-
-app = Flask(__name__)
-
-@app.route('/')
-def home():
-    # تأخير 5 ثواني لحماية DDoS
-    time.sleep(8)
-    return "مرحبًا بك في السيرفر بعد الانتظار."
-
-if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=443)
 
